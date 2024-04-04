0c9d6e4d1ac05f68b37390ce9e9873104c404d3d
yannik.riederer <yannik.riederer@uzh.ch>
2024-04-04 16:16:49 +0200

    styling of welcome page

--- src/components/ui/WelcomePage.tsx
+++ src/components/ui/WelcomePage.tsx
@@ -4,16 +4,18 @@ import "styles/views/WelcomePage.scss"
 const WelcomePage = () => {
   return (
     <div className="page">
-      <div className="logo">
-        <img src="/logo.png" alt="logo"/>
-      </div>
-      <div className="title">
-        <h1>Welcome to RapidTracker!</h1>
-        <h2>please login or register to continue</h2>
-      </div>
-      <div className="buttons">
-        <button type="button">Login!</button>
-        <button type="button">Register!</button>
+      <div className="content">
+        <div className="logo">
+          <img src="/logo.png" alt="logo"/>
+        </div>
+        <div className="title">
+          <h1>Welcome to RapidTracker !</h1>
+          <h2>please login or register to continue</h2>
+        </div>
+        <div className="buttons">
+          <button type="button">Login!</button>
+          <button type="button">Register!</button>
+        </div>
       </div>
     </div>
   );
