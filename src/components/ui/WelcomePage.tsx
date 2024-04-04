e1c8351996e63dc95ac306e4563343a40063464b
yannik.riederer <yannik.riederer@uzh.ch>
2024-04-04 15:02:02 +0200

    welcome page component

--- src/components/ui/WelcomePage.tsx
+++ src/components/ui/WelcomePage.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import "styles/views/WelcomePage.scss"
 
 const WelcomePage = () => {
   return (
-    <div>
-      <h1>Welcome</h1>
+    <div className="page">
+      <div className="logo">
+        <img src="/logo.png" alt="logo"/>
+      </div>
+      <div className="title">
+        <h1>Welcome to RapidTracker!</h1>
+        <h2>please login or register to continue</h2>
+      </div>
+      <div className="buttons">
+        <button type="button">Login!</button>
+        <button type="button">Register!</button>
+      </div>
     </div>
   );
 }
