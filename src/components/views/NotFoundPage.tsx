2c2d7f6f943ac52c2ff8c729d78df7fd7ebf96ec
yannik.riederer <yannik.riederer@uzh.ch>
2024-04-10 17:23:47 +0200

    styled notfoundpage

--- src/components/views/NotFoundPage.tsx
+++ src/components/views/NotFoundPage.tsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 
 const NotFoundPage = () => (
   <BaseContainer>
-    <div className="content">
-      <h1>Page Not Found</h1>
+    <div className="flex flex-col items-center justify-center h-full text-center">
+      <h1 className="text-accent text-4xl">Page Not Found</h1>
       <p>The page you&apos;re looking for does not seem to exist</p>
-      <Link to="/">Go back</Link>
+      <Link to="/" className="hover:underline">Go back</Link>
     </div>
   </BaseContainer>
 );
