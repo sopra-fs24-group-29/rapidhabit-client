ea1274ce7b84e965690e866e9e73e93dfe66495b
Simon Hafner <simon.hafner3@uzh.ch>
2024-04-22 18:15:31 +0200

    Exchanged websocket library
    This update fixes the blank screen error within the app.

--- src/components/views/FeedPage.tsx
+++ src/components/views/FeedPage.tsx
@@ -1,26 +1,25 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import SockJS from "sockjs-client";
-const Stomp = require("@stomp/stompjs");
+import { Client } from "@stomp/stompjs";
 
 const FeedPage = () => {
   const [feedEntries, setFeedEntries] = useState([]);
   const [groupId, setGroupId] = useState("123");
 
   useEffect(() => {
+    const token = localStorage.getItem("token");
     const socket = new SockJS("http://localhost:8080/ws");
     const client = new Client({
       webSocketFactory: () => socket,
       connectHeaders: {
-        Authorization: "hnXAYyVqZu7Vi1j8JUaw3uu-kpc",
+        Authorization: "" + token,
       },
       onConnect: () => {
         console.log("Connected to WS");
         client.subscribe(`/topic/groups/${groupId}/feed`, (message) => {
           const newEntry = JSON.parse(message.body);
-          setFeedEntries((prevEntries) => [newEntry, ...prevEntries]);
+          setFeedEntries((prevEntries) => prevEntries.concat(newEntry));
         });
-
-        // Hier könntest du die initialen Feed-Daten vom Server anfordern.
         fetchInitialFeeds(groupId);
       },
       onDisconnect: () => {
@@ -33,14 +32,29 @@ const FeedPage = () => {
     return () => {
       client.deactivate();
     };
-  }, [groupId]);
+  }, [groupId]); // Stelle sicher, dass alle relevanten Abhängigkeiten hier aufgeführt sind
 
-  // Eine Funktion, um die ersten 20 Feed-Einträge zu laden
   const fetchInitialFeeds = async (groupId) => {
-    const response = await fetch(
-      `http://localhost:8080/api/group/${groupId}/feed`
-    );
-    const data = await response.json();
-    setFeedEntries(data); // Stelle sicher, dass die Daten im richtigen Format sind
+    try {
+      const response = await fetch(
+        `http://localhost:8080/api/group/${groupId}/feed`
+      );
+      const data = await response.json();
+      setFeedEntries(data);
+    } catch (error) {
+      console.error("Failed to fetch initial feeds:", error);
+    }
   };
+
+  return (
+    <div>
+      {feedEntries.map((entry, index) => (
+        <div key={index}>
+          <p>{entry}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
+
+export default FeedPage;
