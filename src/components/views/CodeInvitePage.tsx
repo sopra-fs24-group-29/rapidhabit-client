7dc81aad37c02ac349934f67ee5e3da23f60e542
yannik.riederer <yannik.riederer@uzh.ch>
2024-04-22 21:32:51 +0200

    invite page

--- src/components/views/CodeInvitePage.tsx
+++ src/components/views/CodeInvitePage.tsx
@@ -48,10 +48,10 @@ const CodeInvitePage = () => {
           <div>
             <h3 className="mt-5 text-5xl">{accessCode}</h3>
           </div>
-          <div className="hover:text-red-600">
+          <div>
             <button
               className="cursor-pointer mt-8 text-sm text-accent p-2 outline outline-accent
-              rounded-lg flex items-center"
+              rounded-lg flex items-center transform hover:translate-x-0.5 hover:translate-y-0.5"
             >
               <img src="/clipboard.png" alt="copy-icon" className="w-5 h-5 mr-2" />
               <span>COPY</span>
--- src/components/views/FeedPageTemplate.tsx
+++ src/components/views/FeedPageTemplate.tsx
@@ -15,9 +15,6 @@ const FeedPage = () => {
               group={"G1"}
               color={"bg-green-500"}
               p1={"Lena completed a 30-minute meditation session!"}
-              p2={
-                "Peace and calm, Lena! Who else is finding their inner peace today?"
-              }
             ></FeedBox>
 
             <h2 className="mt-4">Yesterday</h2>
@@ -25,7 +22,6 @@ const FeedPage = () => {
               group={"G2"}
               color={"bg-blue-500"}
               p1={"John reached his daily step goal of 10,000 steps!"}
-              p2={"Way to go, John! Keep those steps coming!"}
             ></FeedBox>
             <FeedBox
               group={"G3"}
@@ -33,23 +29,16 @@ const FeedPage = () => {
               p1={
                 "Welcome to the group, Kim! Let's achieve our goals together!"
               }
-              p2={
-                "Keep up the great work, everyone! Together, we can accomplish amazing things!"
-              }
             ></FeedBox>
             <FeedBox
               group={"G1"}
               color={"bg-green-500"}
               p1={"Alex just completed his workout session!"}
-              p2={
-                "Great job, Alex! You’re inspiring us all to sweat a little more today."
-              }
             ></FeedBox>
             <FeedBox
               group={"G3"}
               color={"bg-purple-500"}
               p1={"Sarah finished reading a chapter of her book!"}
-              p2={"Well done, Sarah! What’s the next chapter about?"}
             ></FeedBox>
 
             <h2 className="mt-4">Recent days</h2>
@@ -57,7 +46,6 @@ const FeedPage = () => {
               group={"G2"}
               color={"bg-blue-500"}
               p1={"Michael practiced playing guitar for 1 hour today!"}
-              p2={"Rock on, Michael! Keep strumming those chords!"}
             ></FeedBox>
             <FeedBox
               group={"G1"}
@@ -65,27 +53,18 @@ const FeedPage = () => {
               p1={
                 "Welcome to the group, Emma! Let's work together to build healthy habits!"
               }
-              p2={
-                "Excited to have you join us, Emma! Let's crush our goals together!"
-              }
             ></FeedBox>
             <FeedBox
               group={"G3"}
               color={"bg-purple-500"}
               p1={"Emily finished her daily yoga session!"}
-              p2={
-                "Congratulations, Emily! Keep up the consistency and feel the benefits of yoga."
-              }
             ></FeedBox>
             <FeedBox
               group={"G2"}
               color={"bg-blue-500"}
               p1={"Joined the group: Michael"}
-              p2={
-                "Welcome aboard, Michael! Together, we'll strive for progress, not perfection."
-              }
             ></FeedBox>
-            <FeedBox group={""} color={""} p1={""} p2={""}></FeedBox>
+            <FeedBox group={""} color={""} p1={""} ></FeedBox>
           </div>
         </div>
       </BaseContainer>
