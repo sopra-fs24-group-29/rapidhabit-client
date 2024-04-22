a25a9032b059ebaa8a11a63da8eebcd0c3c4ad07
yannik.riederer <yannik.riederer@uzh.ch>
2024-04-22 10:31:13 +0200

    added tabbar icons and logic

--- src/components/ui/Tabbar.tsx
+++ src/components/ui/Tabbar.tsx
@@ -1,20 +1,46 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 const TabBar = () => {
   const navigate = useNavigate();
+  const location = useLocation();
+  const [activeTab, setActiveTab] = useState("/app");
+
+  useEffect(() => {
+    setActiveTab(location.pathname);
+  }, [location]);
+
   return (
     <div className="flex flex-row gap-14 lg:gap-48 justify-center fixed bottom-0 w-full bg-input p-8 text-white text-center">
       <div className="px-4 cursor-pointer" onClick={() => navigate("/app")}>
-        <div className="text-xs">icon</div>
-        <div className="text-xs">Home</div>
+        <div>
+          <img
+            className="h-8 w-8"
+            src={activeTab === "/app" ? "/onhome.png" : "/offhome.png"}
+            alt="home tab icon"
+          />
+        </div>
+        <div className={activeTab === "/app" ? "text-xs" : "text-xs text-tab-off"}>Home</div>
       </div>
       <div className="px-4 cursor-pointer" onClick={() => navigate("/feed")}>
-        <div className="text-xs">icon</div>
-        <div className="text-xs">Feed</div>
+        <div>
+          <img
+            className="h-8 w-8"
+            src={activeTab === "/feed" ? "/onfeed.png" : "/offfeed.png"}
+            alt="feed tab icon"
+          />
+        </div>
+        <div className={activeTab === "/feed" ? "text-xs" : "text-xs text-tab-off"}>Feed</div>
       </div>
       <div className="px-4 cursor-pointer" onClick={() => navigate("/profile")}>
-        <div className="text-xs">icon</div>
-        <div className="text-xs">Profile</div>
+        <div>
+          <img
+            className="h-8 w-8"
+            src={activeTab === "/profile" ? "/onprofile.png" : "/offprofile.png"}
+            alt="profile tab icon"
+          />
+        </div>
+        <div className={activeTab === "/profile" ? "text-xs" : "text-xs text-tab-off"}>Profile</div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ export const theme = {
       input: "#1A1A1A",
       "dark-green": "#0B261A",
       "light-green": "#57BF84",
+      "tab-off": "#8A8A8A"
     },
     width: {
       "custom-236": "236px",
