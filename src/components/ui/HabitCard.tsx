499360613fc1b927c2eedbd7833b7303f4deed8f
Raksmey Oum <>
2024-04-23 11:29:35 +0200

    Handled error for trying to check habits that arent due

--- src/components/ui/HabitCard.tsx
+++ src/components/ui/HabitCard.tsx
@@ -39,18 +39,22 @@ const HabitCard = (props: HabitCardProps) => {
       <div className="bg-dark-green p-4 rounded-lg relative">
         <div className="flex flex-row justify-between">
           <div>🔥{props.habit.streaks}</div>
-          <button
-            className={clsx(
-              "p-4 absolute right-0 top-0",
-              isUpdating && "opacity-20 cursor-progress"
-            )}
-            onClick={(event) => {
-              event.preventDefault();
-              toggleHabitChecked();
-            }}
-          >
-            <Check isChecked={props.habit.checked} />
-          </button>
+          {props.habit.onSchedule ? (
+            <button
+              className={clsx(
+                "p-4 absolute right-0 top-0",
+                isUpdating && "opacity-20 cursor-progress"
+              )}
+              onClick={(event) => {
+                event.preventDefault();
+                toggleHabitChecked();
+              }}
+            >
+              <Check isChecked={props.habit.checked} />
+            </button>
+          ) : (
+            <span className="text-sm opacity-60">Not due today</span>
+          )}
         </div>
         <div className="pt-6 pb-2 text-xl font-bold truncate">
           {props.habit.name}
