b6fa7a24c7bf96435b091671236e5cdcc400eed4
yannik.riederer <yannik.riederer@uzh.ch>
2024-04-24 13:48:46 +0200

    completed delete group page

--- src/components/views/DeleteGroupPage.tsx
+++ src/components/views/DeleteGroupPage.tsx
@@ -1,28 +1,44 @@
 import BaseContainer from "components/ui/BaseContainer";
 import NavigationBar from "components/ui/NavigationBar.tsx";
 import TabBar from "../ui/Tabbar.tsx";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import { api } from "../../helpers/api.ts";
 
 const DeleteGroupPage = () => {
   const { groupId } = useParams();
+  const navigate = useNavigate();
+
+  const deleteGroup = async () => {
+    try {
+      await api.delete(
+        `/groups/${groupId}`,
+        { headers: { Authorization: localStorage.getItem("token"), }, });
+      console.log("Group deleted successfully!");
+      navigate("/app");
+    } catch (error) {
+      console.error("Error deleting group:", error);
+    }
+  };
 
   return (
     <div>
       <BaseContainer>
         <NavigationBar
-          backUrl="/app/${groupId}"
+          backUrl={`/app/${groupId}/settings`}
           title="Delete group"
           rightAction={
             <button
               className="text-white font-bold"
               type="button"
+              onClick={deleteGroup}
             >
               Confirm
             </button>
           }
         />
-        <div className="flex flex-col items-center justify-start mt-8">
-
+        <div className="flex flex-col text-left justify-start mt-8 ">
+          <p>Are you sure you want to delete this group?</p>
+          <p>All data associated with this group will be permanently deleted!</p>
         </div>
       </BaseContainer>
       <TabBar />
