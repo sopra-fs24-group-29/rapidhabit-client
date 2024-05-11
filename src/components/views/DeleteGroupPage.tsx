46b9323d4793eb046b8bd68bc37430fba7f5a1ca
Raksmey Oum <>
2024-05-11 11:08:53 +0200

    algned text

--- src/components/views/DeleteGroupPage.tsx
+++ src/components/views/DeleteGroupPage.tsx
@@ -1,8 +1,8 @@
 import BaseContainer from "components/ui/BaseContainer";
 import NavigationBar from "components/ui/NavigationBar.tsx";
-import TabBar from "../ui/Tabbar.tsx";
 import { useNavigate, useParams } from "react-router-dom";
 import { api } from "../../helpers/api.ts";
+import TabBar from "../ui/Tabbar.tsx";
 
 const DeleteGroupPage = () => {
   const { groupId } = useParams();
@@ -10,9 +10,9 @@ const DeleteGroupPage = () => {
 
   const deleteGroup = async () => {
     try {
-      await api.delete(
-        `/groups/${groupId}`,
-        { headers: { Authorization: localStorage.getItem("token"), }, });
+      await api.delete(`/groups/${groupId}`, {
+        headers: { Authorization: localStorage.getItem("token") },
+      });
       console.log("Group deleted successfully!");
       navigate("/app");
     } catch (error) {
@@ -36,9 +36,11 @@ const DeleteGroupPage = () => {
             </button>
           }
         />
-        <div className="flex flex-col text-left justify-start mt-8 ">
+        <div className="flex flex-col text-left p-6 ">
           <p>Are you sure you want to delete this group?</p>
-          <p>All data associated with this group will be permanently deleted!</p>
+          <p>
+            All data associated with this group will be permanently deleted!
+          </p>
         </div>
       </BaseContainer>
       <TabBar />
