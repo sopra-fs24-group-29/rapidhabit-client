9879d378fd2a75d8b2c01068600a01be471037d2
Raksmey Oum <>
2024-05-22 01:38:33 +0200

    add date to feed features

--- src/components/ui/FeedBox.tsx
+++ src/components/ui/FeedBox.tsx
@@ -1,17 +1,32 @@
+import React from "react";
+
 interface FeedProps {
   group: string;
   color: string;
   p1: string;
   p2?: string;
+  dateTime: string;
 }
 
-const FeedBox = (props: FeedProps) => {
+const FeedBox: React.FC<FeedProps> = (props) => {
+  const formattedDateTime = new Date(props.dateTime).toLocaleDateString(
+    "en-US",
+    {
+      day: "numeric",
+      month: "short",
+      year: "numeric",
+    }
+  );
+
   return (
     <div
       className={`w-full ${props.color} bg-opacity-50 rounded-lg shadow-lg mt-2 p-4 flex flex-col justify-between`}
     >
       <div className="mb-2">
-        <h3 className="text-lg font-bold">{props.group}</h3>
+        <div className="flex flex-row justify-between">
+          <h3 className="text-lg font-bold">{props.group}</h3>
+          <div className="pt-[3px] pr-1">{formattedDateTime}</div>
+        </div>
         <p className="text-sm">{props.p1}</p>
         <p className="text-sm">{props.p2}</p>
       </div>
